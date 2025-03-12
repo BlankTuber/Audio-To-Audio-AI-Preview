0c9d92954c062c2461adbe5a4ab8302af435c6b4
Blank <blank@BlankPC.localdomain>
2025-03-12 12:02:33 +0100

    Swapping to py

--- discord_bot/config/setConfig.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-global.TOKEN = process.env.DISCORD_TOKEN;
-global.CLIENT = process.env.CLIENT_ID;
-global.GUILD = process.env.GUILD_ID;
-global.OLLAMA = process.env.OLLAMA_API_HOST;
-global.MODEL = process.OLLAMA_DEFAULT_MODEL;
-
-module.exports = global;
