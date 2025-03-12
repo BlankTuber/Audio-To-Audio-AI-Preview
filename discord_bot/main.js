bd449e5c82521a07f8ffb155f9a290d8b53c7393
Blank <blank@BlankPC.localdomain>
2025-03-12 11:37:39 +0100

    Updates

--- discord_bot/main.js
+++ discord_bot/main.js
@@ -1,1 +1,11 @@
-require("dotenv").config();
+require("./config/setConfig");
+
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
